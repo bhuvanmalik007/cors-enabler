f664e2720f2808fae2f6098413e0d779fab74b4a
bhuvan malik <bhuvanmalik007@gmail.com>
2020-05-18 05:47:19 -0400

    added supprt for multiple urls

--- handler.js
+++ handler.js
@@ -1,36 +1,46 @@
-'use strict';
-const fetch = require('node-fetch');
+"use strict";
+const fetch = require("node-fetch");
 
-module.exports.corsEnabler = async event => {
-  if(event.queryStringParameters && event.queryStringParameters.url){
-    const url = event.queryStringParameters.url;
-    const response = await fetch(event.queryStringParameters.url);
-    const json = await response.json();
+module.exports.corsEnabler = async (event) => {
+  if (event.queryStringParameters && event.queryStringParameters.url) {
+    const urls = event.queryStringParameters.url.split(",");
+    const responses = await Promise.all(
+      urls.map((url) =>
+        fetch(url).then(function (response) {
+          return response.json();
+        })
+      )
+    )
+      .then(function (data) {
+        return data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // const response = await fetch(event.queryStringParameters.url);
+    // const json = await response.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          error: false,
-          message: "URL found: " + url,
-          data: json,
-        }
-      ),
+      body: JSON.stringify({
+        success: true,
+        message: "URL(s) found: " + urls,
+        data: responses,
+      }),
       headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+        "Access-Control-Allow-Origin": "*",
+      },
     };
-  }
-  else{
+  } else {
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          error: true,
-          message: "URL not found."
-        }
-      ),
+      body: JSON.stringify({
+        success: false,
+        message: "No url found.",
+      }),
       headers: {
-        'Access-Control-Allow-Origin': '*'
+        "Access-Control-Allow-Origin": "*",
       },
     };
   }
